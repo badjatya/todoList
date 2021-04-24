f0334747c02128170bfa8710573c5558e35970b1
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-04-25 04:45:51 +0530

    Style Update

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#E8EAED',
   },
   tasksWrapper: {
-    paddingTop:80,
+    paddingTop:70,
     paddingHorizontal:20,
   },
   sectionTitle: {
